82682ebc7772cb1e101855e13873288b25ff57a5
Sergey Zelenovsky <zelnovskiygoodman454@gmail.com>
2022-07-05 17:52:19 +1000

    UPDATE: Auth constraints.

--- src/main/java/com/greatestquotes/controllers/AuthController.java
+++ src/main/java/com/greatestquotes/controllers/AuthController.java
@@ -4,6 +4,7 @@ import com.greatestquotes.utils.HashCode;
 import javafx.fxml.FXML;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 
 public class AuthController extends BaseController {
 
@@ -12,6 +13,9 @@ public class AuthController extends BaseController {
 
     @FXML
     protected PasswordField passwordField;
+
+    @FXML
+    protected Text messageText;
 
     @FXML
     protected void onGuestButtonClick() {
@@ -27,7 +31,13 @@ public class AuthController extends BaseController {
     protected void onSignInButtonClick() {
         String login = loginField.getText();
         String password = passwordField.getText();
+
+        if (login.length() == 0 || password.length() == 0)
+            messageText.setText("Please, fill the fields.");
+
         if (user.auth(login, password))
             rootApp.showMainWindow();
+        else
+            messageText.setText("Wrong login or password!");
     }
 }
