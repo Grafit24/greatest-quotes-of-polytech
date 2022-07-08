a26cd869067dfa4ee03e5f5d126f970e3ab18d50
Sergey Zelenovsky <zelnovskiygoodman454@gmail.com>
2022-07-08 23:53:56 +1000

    ADD: Quotes parsing.

---
+++ src/main/java/com/greatestquotes/models/Quotes.java
@@ -0,0 +1,48 @@
+package com.greatestquotes.models;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Quotes extends ArrayList<Quote> {
+    public void parseRead(User user) {
+        String query = "SELECT DISTINCT quotes.id as id, quote, teacher, subject, date, login as owner, " +
+                "BIT_OR(op_read) as r, BIT_OR(op_write) as w, BIT_OR(op_delete) as d " +
+                "FROM quotes INNER JOIN access INNER JOIN users ON quotes.id = access.id_record AND users.id=quotes.id_creator " +
+                "WHERE id_creator=? OR id_role IN " + user.getRoles().createTemplate4Query() + " OR " +
+                "(id_role=3 AND (SELECT COUNT(*) > 0 FROM moderators_groups WHERE id_moderator=? " +
+                "AND id_group IN (SELECT id_role FROM credentials WHERE id_user=id_creator)) AND ?) " +
+                "GROUP BY id, quote, teacher, subject, date, owner;";
+
+        try {
+            Connection c = DBHandler.getConnection();
+            PreparedStatement p = c.prepareStatement(query);
+            p.setLong(1, user.getID());
+            int i = 2;
+            for (Role role : user.getRoles())
+                p.setLong(i++, role.id());
+            p.setLong(i++, user.getID());
+            p.setBoolean(i, true);
+            ResultSet result = p.executeQuery();
+
+            while (result.next()) {
+                long id = result.getLong("id");
+                String quote = result.getString("quote");
+                String teacher = result.getString("teacher");
+                String subject = result.getString("subject");
+                Date date = result.getDate("date");
+                String owner = result.getString("owner");
+                boolean r = result.getBoolean("r");
+                boolean w = result.getBoolean("w");
+                boolean d = result.getBoolean("d");
+                Quote q = new Quote(id, quote, teacher, subject, date, owner, r, w, d);
+                this.add(q);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
